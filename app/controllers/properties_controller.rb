59f80066ea25c0dc5ed7120157373a7dbb0fc076
Serena Green <serenafgreen@gmail.com>
2023-11-20 22:12:59 -0800

    adds create request with post convention and action

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -10,4 +10,18 @@ class PropertiesController < ApplicationController
     render :show
   end
 
+  def create
+    @property = Property.create(
+    description: params["description"],
+    year_built: params["year_built"],
+    square_feet: params["square_feet"],
+    bedrooms: params["bedrooms"],
+    bathrooms: params["bathrooms"],
+    floors: params["floors"],
+    availability: params["availability"],
+    address: params["address"],
+    price: params["price"],
+    )
+    render :show
+    end
 end
