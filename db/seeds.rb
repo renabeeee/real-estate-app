7ea50ae52092269c3fc3fe5bd41615de7b25fccc
Serena Green <serenafgreen@gmail.com>
2023-11-20 22:31:30 -0800

    adds seed dump gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,7 @@
-property = Property.create(
-  description: "Lovely, quiet home in Calabasa, CA",
-  year_built: "1997",
-  square_feet: "10,000",
-  bedrooms: "6",
-  bathrooms: "4",
-  floors: "2",
-  availability: "January 2024",
-  address: "32098 Willcrest Avenue, Beverly Hills, CA 90021",
-  price: 2800000.00
-)
-
-property = Property.create(
-  description: "Vibrant condo in the famous Hollywood Hills",
-  year_built: "1985",
-  square_feet: "8,500",
-  bedrooms: "4",
-  bathrooms: "3",
-  floors: "2",
-  availability: "February 2024",
-  address: "2088 Lulu Glen Drive, Los Angeles, CA 90068",
-  price: 2100000.00
-)
-
-property = Property.create(
-  description: "Cozy beachside villa in Malibu",
-  year_built: "1999",
-  square_feet: "4,500",
-  bedrooms: "4",
-  bathrooms: "2",
-  floors: "3",
-  availability: "January 2024",
-  address: "10073 Ocean Avenue, Malibu, CA 90401",
-  price: 1800000.00
-)
-
-property = Property.create(
-  description: "Mini mansion in the heart of LA",
-  year_built: "1978",
-  square_feet: "12,800",
-  bedrooms: "6",
-  bathrooms: "5",
-  floors: "3",
-  availability: "January 2024",
-  address: "730 Linden Avenue, Los Angeles, CA 90004",
-  price: 3700000.00
-)
+Property.create!([
+  {description: "Lovely, quiet home in Calabasa, CA", year_built: "1997", square_feet: "10,000", bedrooms: "6", bathrooms: "4", availability: "January 2024", address: "32098 Willcrest Avenue, Beverly Hills, CA 90021", price: "2800000.0"},
+  {description: "Vibrant condo in the famous Hollywood Hills", year_built: "1985", square_feet: "8,500", bedrooms: "4", bathrooms: "3", availability: "February 2024", address: "2088 Lulu Glen Drive, Los Angeles, CA 90068", price: "2100000.0"},
+  {description: "Cozy beachside villa in Malibu", year_built: "1999", square_feet: "4,500", bedrooms: "4", bathrooms: "2", availability: "January 2024", address: "10073 Ocean Avenue, Malibu, CA 90401", price: "1800000.0"},
+  {description: "Mini mansion in the heart of LA", year_built: "1978", square_feet: "12,800", bedrooms: "6", bathrooms: "5", availability: "January 2024", address: "730 Linden Avenue, Los Angeles, CA 90004", price: "3700000.0"},
+  {description: "Gorgeous home for those who love the outdoors", year_built: "2002", square_feet: "7,000", bedrooms: "3", bathrooms: "3", availability: "March 2023", address: "2432 Hacienda Boulevard, Los Angeles, CA 90302", price: "1200000.0"}
+])
